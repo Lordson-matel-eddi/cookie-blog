dba97cb8195464322eb1828b8954e3d0571b09e6
lordson <80492739+Lordson-matel-eddi@users.noreply.github.com>
2023-05-29 01:13:40 +0000

    final cmt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import './globals.css'
-
+import { useEffect, useState } from 'react'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
 
 
 export default function RootLayout({
@@ -11,14 +13,40 @@ export default function RootLayout({
 }) {
 
 
-const signUp = () => {
-    supabase.signUp({
+
+  const [ supabase ] = useState(() => createPagesBrowserClient())
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const { data: { subscription } } =  supabase.auth.onAuthStateChange(() => {
+      router.refresh()
+    })
+    return () => {
+      subscription.unsubscribe()
+    }
+  }, [supabase, router])
+
+
+  const signUp = () => {
+    supabase.auth.signUp({
     email: "lordsonmateleddi@gmail.com",
-    password: 'fullsatrk'
+    password: 'fullstark'
   })
-    
-}
-        
+ }
+
+
+ const signIn = () => {
+  supabase.auth.signInWithPassword({
+    email: 'lordsonmateleddi@gmail.com',
+    password: 'fullstark'
+  })
+ }
+
+
+ const signOut = () => {
+  supabase.auth.signOut()
+ }
         
     
     
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { createClient } from '@supabase/supabase-js'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 
 export default async function Post() {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  )
+  const supabase = createServerComponentClient({
+    cookies
+  })
+
   const { data: posts } = await supabase.from('posts').select('id, title, created_at' )
   return <pre>{ JSON.stringify(posts, null, 2) }</pre>
 }
